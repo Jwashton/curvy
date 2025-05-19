77ed9551b846607bea2adb381e517d8e5220d1bd
William Ashton <William@AshtonFam.org>
2025-05-18 21:54:49 -0400

    Add functions to find c_1 and c_2

--- src/curvy.mjs
+++ src/curvy.mjs
@@ -13,3 +13,26 @@ export const C = function C(t) {
 export const D = function D(t) {
   return t ** 3;
 };
+
+export const g = function g(s, c_1, c_2, e, t) {
+  return s * A(t) + c_1 * B(t) + c_2 * C(t) + e * D(t);
+};
+
+const E = function E(s, e, g, t) {
+  return (g - s * A(t) - e * D(t)) / B(t);
+};
+
+const F = function F(s, e, g_2, t_1, t_2) {
+  return (C(t_1) * (g_2 - s * A(t_2) - e * D(t_2))) / (B(t_1) * C(t_2));
+};
+
+export const findC1 = function findC1(s, e, g_1, g_2, t_1, t_2) {
+  const e_for_c = E(s, e, g_1, t_1);
+  const f_for_c = F(s, e, g_2, t_1, t_2);
+
+  return ((e_for_c - f_for_c) * B(t_1) * C(t_2)) / (B(t_1) * C(t_2) - B(t_2) * C(t_1));
+};
+
+export const findC2 = function findC2(s, e, g, t, c_1) {
+  return (g - s * A(t) - c_1 * B(t) - e * D(t)) / C(t);
+};
--- test/curvyTest.mjs
+++ test/curvyTest.mjs
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert';
 
-import { A, B, C, D } from '../src/curvy.mjs';
+import { A, B, C, D, g, findC1, findC2 } from '../src/curvy.mjs';
 
 const assertWithin = function assertWithin(
   testValue,
@@ -57,6 +57,56 @@ test('D for 1', (t) => {
   assert.strictEqual(D(1.0), 1);
 });
 
-test('D for 1', (t) => {
+test('D for 1/3rd', (t) => {
   assertWithin(D(1.0 / 3.0), 1.0 / 27.0);
+});
+
+test('g for a simple curve, at t=0', (t) => {
+  assertWithin(g(0.0, 1.0, 2.0, 3.0, 0.0), 0.0);
+});
+
+test('g for a simple curve, at t=1', (t) => {
+  assertWithin(g(0.0, 1.0, 2.0, 3.0, 1.0), 3.0);
+});
+
+test('g for a simple curve, at t=1/3', (t) => {
+  assertWithin(g(0.0, 1.0, 2.0, 3.0, 1.0 / 3.0), 1.0);
+});
+
+test('g for a simple curve, at t=2/3', (t) => {
+  assertWithin(g(0.0, 1.0, 2.0, 3.0, 2.0 / 3.0), 2.0);
+});
+
+test('finding c_1 for two points on a curve', (t) => {
+  const s = 3.5;
+  // We'll calculate two g values, and then use those
+  // calculated g values to show we can get this c_1
+  // value back.
+  const c_1 = 2.6;
+  const c_2 = 6.2;
+  const e = 10.0;
+  
+  const t_1 = 0.2;
+  const g_1 = g(s, c_1, c_2, e, t_1);
+  const t_2 = 0.7;
+  const g_2 = g(s, c_1, c_2, e, t_2);
+
+  assertWithin(findC1(s, e, g_1, g_2, t_1, t_2), c_1);
+});
+
+test('finding c_2 for two points on a curve', (t) => {
+  const s = 3.5;
+  // We'll calculate two g values, and then use those
+  // calculated g values to show we can get this c_1
+  // value back.
+  const c_1 = 2.6;
+  const c_2 = 6.2;
+  const e = 10.0;
+  
+  const t_1 = 0.2;
+  const g_1 = g(s, c_1, c_2, e, t_1);
+  const t_2 = 0.7;
+  const g_2 = g(s, c_1, c_2, e, t_2);
+
+  assertWithin(findC2(s, e, g_2, t_2, c_1), c_2);
 });
